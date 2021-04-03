02a96419e1a8d2aa49f45f78f4758b93f0e1a337
Adrien Champredonde <adrien.champredonde@gmail.com>
2021-04-03 14:25:41 +0200

    Init project with spring boot and angular

---
+++ src/main/java/com/project/isilearn/IsilearnApplication.java
@@ -0,0 +1,13 @@
+package com.project.isilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsilearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsilearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/isilearn/IsilearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.isilearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsilearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
